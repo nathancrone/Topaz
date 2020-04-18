0f5609b1aac8c959daf93ddd78443d7d4f5506c7
Nathan Crone <nathan.crone@gmail.com>
2020-04-17 20:25:00 -0500

    adding street territory

---
+++ Topaz.UI.Razor/Pages/StreetTerritory/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Topaz.Common.Models;
+using Topaz.Data;
+
+namespace Topaz.UI.Razor.Pages.StreetTerritory
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Topaz.Data.TopazDbContext _context;
+
+        public IndexModel(Topaz.Data.TopazDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<StreetTerritory> StreetTerritory { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            StreetTerritory = await _context.StreetTerritories.ToListAsync();
+        }
+    }
+}
